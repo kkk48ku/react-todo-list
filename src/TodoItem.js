462b24ede6730d10b24b1f54d4c28d2710fff833
yumi <yikeyumi65@gmail.com>
2019-10-19 17:35:50 +0800

    使用生命周期函数优化代码及mock数据

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,30 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
 
 export default class TodoItem extends Component {
 	constructor(props) {
-		super(props)
-		this.handleItemDelete = this.handleItemDelete.bind(this)
+		super(props);
+		this.handleItemDelete = this.handleItemDelete.bind(this);
 	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		if (this.props.item !== nextProps.item) return true;
+		return false;
+	}
+
 	render() {
-		const { item } = this.props
-		return <li onClick={this.handleItemDelete}>{item}</li>
+		const { item } = this.props;
+		return <li onClick={this.handleItemDelete}>{item}</li>;
 	}
+
 	handleItemDelete() {
-		const { handleItemDelete, index } = this.props
-		handleItemDelete(index)
+		const { handleItemDelete, index } = this.props;
+		handleItemDelete(index);
 	}
 }
+
+TodoItem.propTypes = {
+	item: propTypes.string.isRequired,
+	handleItemDelete: propTypes.func,
+	index: propTypes.number
+};
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,20 +1,22 @@
-import React, { Component, Fragment } from "react"
-import TodoItem from "./TodoItem"
+import React, { Component, Fragment } from 'react';
+import TodoItem from './TodoItem';
+import axios from 'axios';
 
 export default class TodoList extends Component {
 	constructor(props) {
-		super(props)
+		super(props);
+		// 当组件的state或者props发生改变的时候，render函数就会重新执行
 		this.state = {
-			inputValue: "",
+			inputValue: '',
 			list: []
-		}
-		this.handleBtnClick = this.handleBtnClick.bind(this)
-		this.handleItemDelete = this.handleItemDelete.bind(this)
-		this.handleInputChange = this.handleInputChange.bind(this)
+		};
+		this.handleBtnClick = this.handleBtnClick.bind(this);
+		this.handleItemDelete = this.handleItemDelete.bind(this);
+		this.handleInputChange = this.handleInputChange.bind(this);
 	}
 
 	render() {
-		const { inputValue, list } = this.state
+		const { inputValue, list } = this.state;
 		return (
 			<Fragment>
 				<div>
@@ -23,34 +25,47 @@ export default class TodoList extends Component {
 				</div>
 				<ul>{this.getItem(list)}</ul>
 			</Fragment>
-		)
+		);
+	}
+
+	componentDidMount() {
+		axios
+			.get('/api/todoList')
+			.then(res => {
+				this.setState(() => ({
+					list: [...res.data]
+				}));
+			})
+			.catch(err => {
+				throw new Error(err);
+			});
 	}
 
 	getItem(list) {
 		return list.map((item, index) => {
-			return <TodoItem item={item} index={index} key={index} handleItemDelete={this.handleItemDelete} />
-		})
+			return <TodoItem item={item} index={index} key={index + item} handleItemDelete={this.handleItemDelete} />;
+		});
 	}
 
 	handleInputChange(e) {
-		const inputValue = e.target.value
+		const inputValue = e.target.value;
 		this.setState(() => ({
 			inputValue
-		}))
+		}));
 	}
 
 	handleBtnClick() {
 		this.setState(prevState => ({
 			list: [...prevState.list, prevState.inputValue],
-			inputValue: ""
-		}))
+			inputValue: ''
+		}));
 	}
 
 	handleItemDelete(idx) {
 		this.setState(prevState => {
-			const list = [...prevState.list]
-			list.splice(idx, 1)
-			return { list }
-		})
+			const list = [...prevState.list];
+			list.splice(idx, 1);
+			return { list };
+		});
 	}
 }
